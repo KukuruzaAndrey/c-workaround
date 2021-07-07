dafe692dda74f01717d7011397907bccf17159d4
Andrii Kukuruza <kukuruza.a@ajax.systems>
2021-07-07 19:44:01 +0300

    paly

--- 19-write-to-struct-memory.c
+++ 19-write-to-struct-memory.c
@@ -1,10 +1,28 @@
+#include "stdio.h"
+#include "string.h"
+
 struct st {
-  int a;
-  int b;
+    int a;
+    int b;
 };
 
 int main() {
-  struct st c;
-  c = (st){1, 2};
-  printf("%d %d", c.a, c.b);
+    struct st c;
+    c = (struct st) {1, 2};
+    printf("%d %d\n", c.a, c.b);
+    struct st *pointer = &c;
+    char *p = (char *) pointer;
+    for (int i = 0; i < 8; ++i) {
+        char byte = *p;
+        printf("%d\n", byte);
+        p++;
+    }
+    *p = 0xAF;
+    printf("%d %d\n", c.a, c.b);
+    *p = (char *) pointer;
+    for (int i = 0; i < 8; ++i) {
+        char byte = *p;
+        printf("%d\n", byte);
+        p++;
+    }
 }
