614a5d8cce4eca6eeddbbbf37f2bd387fbcc5826
Andrii Kukuruza <kukuruza.a@ajax.systems>
2021-07-08 19:31:43 +0300

    test

--- 19-write-to-struct-memory.c
+++ 19-write-to-struct-memory.c
@@ -8,21 +8,47 @@ struct st {
 
 int main() {
     struct st c;
-    c = (struct st) {1, 2};
-    printf("%d %d\n", c.a, c.b);
     struct st *pointer = &c;
+    printf("%p %ld\n", pointer, sizeof(pointer));
     char *p = (char *) pointer;
-    for (int i = 0; i < 8; ++i) {
-        char byte = *p;
-        printf("%d\n", byte);
-        p++;
-    }
-    *p = 0xAF;
+    printf("%p %ld\n", p, sizeof(p));
+
+    c = (struct st) {10, 2};
     printf("%d %d\n", c.a, c.b);
-    *p = (char *) pointer;
     for (int i = 0; i < 8; ++i) {
-        char byte = *p;
-        printf("%d\n", byte);
-        p++;
+        printf("%d\n", *(p+i));
     }
+    printf("%p %ld\n", p, sizeof(p));
+    char src[] = "dUdU";
+    memcpy (p, src, 4);
+
+
+
+    printf("%p %ld\n", p, sizeof(p));
+    printf("%d %d\n", c.a, c.b);
+    for (int i = 0; i < 8; ++i) {
+        printf("%d\n", *(p+i));
+    }
+
+//    printf("=============\n");
+//    int x = 1, y, z = 3;
+//    int *p, *q;
+//
+//    p = &x;
+//    printf("%d\n", *p); // 1
+//
+//    y = *p;
+//    printf("%d\n", y); // 1
+//
+//    *p = 0;
+//    printf("%d %d\n", x, y); // 0 1
+//
+//    q = &z;
+//    printf("%d\n", *q); // 3
+//
+//    p = q;
+//    *p = 4;
+//    printf("%d\n", z); // 4
+//
+//    printf("%p %p\n", p, q); // p == q
 }
