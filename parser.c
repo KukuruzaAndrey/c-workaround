56c12039b7b84c5ccbc134fc467309f6e4e1cf6a
KukuruzaAndrey <andreyku6@gmai.com>
2021-10-23 12:49:03 +0300

    a

--- parser.c
+++ parser.c
@@ -10,8 +10,9 @@ enum state {
 
 struct parser {
   enum state st;
-  char command_ID;
+  char cmd;
   int length;
+  int data_index;
   char data[18];
 };
 
@@ -21,10 +22,47 @@ int parse(char input) {
   switch(parser->st) {
     case SEARCHING_START:
       if (input == '[') {
-        parser->state = SEARCHING_CMD;
+        parser->st = SEARCHING_CMD;
       }
       break;
-    
+    case SEARCHING_CMD:
+      if (input >= 'A' && input <= 'Z') {
+        parser->cmd = input;
+        parser->st = SEARCHING_LEN;
+      } else if (input == '[') {
+        parser->st = SEARCHING_CMD;
+      } else {
+        parser->st = SEARCHING_START;
+      }
+      break;
+    case SEARCHING_LEN:
+      if (input >= '0' && input <= '9') {
+        parser->length = input - '0';
+        parser->data_index = 0;
+        parser->st = GETTING_DATA;
+      } else if (input == '[') {
+        parser->st = SEARCHING_CMD;
+      } else {
+        parser->st = SEARCHING_START;
+      }
+      break;
+    case GETTING_DATA:
+      if ((input >= '0' && input <= '9') ||
+          (input >= 'a' && input <= 'f') ||
+          (input >= 'A' && input <= 'F')) {
+        if (2*parser->length - parser->data_index >= 1) {
+          parser->data[parser->data_index] = input;
+          parser->data_index += 1;
+          parser->state
+        } el
+        parser->length = input - '0';
+        parser->st = GETTING_DATA;
+      } else if (input == '[') {
+        parser->st = SEARCHING_CMD;
+      } else {
+        parser->st = SEARCHING_START;
+      }
+      break;
   }
 }
 int main() {
