fd963e61fd71693ad30642a17273bbd73fa56b18
KukuruzaANdrey <andreyku6@gmail.com>
2021-11-04 19:41:47 +0200

    3.03

--- 3.03-expand.c
+++ 3.03-expand.c
@@ -7,55 +7,42 @@
 int in = 0;
 
 char *putRange(char start, char end, char *dest) {
-    if (end > start) {
-        for (int i = 0; i <= end - start; ++i) {
-            dest[i] = start + i;
-        }
-        return dest + end - start + 1;
-    } else {
-        for (int i = 0; i <= start - end; ++i) {
-            dest[i] = start - i;
-        }
-        return dest + start - end + 1;
+    unsigned len = (end > start) ? end - start : start - end;
+    int sign = (end > start) ? 1 : -1;
+    unsigned i = 0;
+    while (i <= len) {
+        *(dest++) = start + i * sign;
+        i++;
     }
+    return dest;
+
+}
+
+unsigned isRange(char *start) {
+    return (isdigit(*start) && (*(start + 1) == '-') && isdigit(*(start + 2))) ||
+           (isupper(*start) && (*(start + 1) == '-') && isupper(*(start + 2))) ||
+           (islower(*start) && (*(start + 1) == '-') && islower(*(start + 2)));
 }
 
 void expand(char *s1, char *s2) {
-    unsigned i = 0;
-    while (s2[i] != '\0') {
-        if (isdigit(s2[i])) {
-            if ((s2[i + 1] == '-') && isdigit(s2[i + 2])) {
-                s1 = putRange(s2[i], s2[i + 2], s1);
-                i += 3;
-                continue;
-            }
+    while (*s2 != '\0') {
+        if (isRange(s2)) {
+            s1 = putRange(*s2, *(s2 + 2), s1);
+            s2 += 3;
+            continue;
         }
-        if (isupper(s2[i])) {
-            if ((s2[i + 1] == '-') && isupper(s2[i + 2])) {
-                s1 = putRange(s2[i], s2[i + 2], s1);
-                i += 3;
-                continue;
-            }
-        }
-        if (islower(s2[i])) {
-            if ((s2[i + 1] == '-') && islower(s2[i + 2])) {
-                s1 = putRange(s2[i], s2[i + 2], s1);
-                i += 3;
-                continue;
-            }
-        }
-        *s1 = s2[i];
+        *s1 = *s2;
         s1++;
-        i++;
+        s2++;
     }
     *s1 = '\0';
 }
 
 int main() {
 
-    char *compressed[] = {"abcd", "a", "-sds", "a-z", "G-A", "7-0a-fA-N", "abcd-a", "-7-1-a-c", "a-a"};
+    char *compressed[] = {"abcd", "a", "-sds", "a-z", "G-A", "7-0a-fA-N", "abcd-a",
+                          "-7-1-a-c", "a-a", "a-0-6b--pq-w9-0-3"};
     char s1[100];
-//    putRange('0', '9', s1);
     for (int i = 0; i < len(compressed); ++i) {
         expand(s1, compressed[i]);
         printf("%s - %s\n", compressed[i], s1);
